d77f145be7797bb46a4d25acf341a348b3313907
cameronwp <cameron.w.pittman@gmail.com>
2017-12-04 17:48:28 -0600

    fix deploy

--- README.md
+++ README.md
@@ -4,12 +4,26 @@ Personal site and such.
 
 ## New Post
 
-Run `yarn now` to get a local ISO string of the datetime. Automatically puts it in your clipboard so you can copy into the new post.
+Run `yarn now` to get a local ISO string of the datetime. Automatically puts it in your clipboard so you can paste into the new post.
 
 ## Running in development
 
 `gatsby develop`
 
-## Updating
+## Publishing
 
 Merges to `master` automatically get published.
+
+Running bin/deploy.js uploads public/ to S3 and invalidates the CloudFront cache.
+
+You can run the deploy script locally. Create a `.env` file in the root of the repo with the following environment variables:
+
+```sh
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_REGION=
+BUCKET=
+CLOUDFRONT_DISTRIBUTION_ID=
+```
+
+If you set up a CircleCI deployment, you'll need to set the aforementioned environment variables in the settings.
--- bin/deploy.js
+++ bin/deploy.js
@@ -12,61 +12,69 @@ AWS.config.apiVersions = {
   s3: '2006-03-01',
   cloudfront:'2017-03-25'
 };
-AWS.config.update({region: 'us-east-1'});
+AWS.config.update({region: process.env.AWS_REGION});
 
-/* upload new files */
+function uploadFiles() {
+  return new Promise((resolve, reject) => {
+    const awsS3Client = new AWS.S3();
+    const config = {
+      s3Client: awsS3Client
+    };
+    const client = s3.createClient(config);
 
-const awsS3Client = new AWS.S3();
+    const uploader = client.uploadDir({
+      localDir: "public",
+      deleteRemoved: true,
+      s3Params: {
+        Bucket: process.env.BUCKET
+      }
+    });
+    uploader.on('error', function(err) {
+      console.error(err);
+      reject(err)
+    });
+    uploader.on('progress', function() {
+      if (process.env.CIRCLECI !== 'true') {
+        const percent = `${((uploader.progressAmount / uploader.progressTotal) * 100).toFixed(2) || 0}%`;
+        console.log(percent);
+      }
+    });
+    uploader.on('end', function() {
+      resolve();
+    });
+  })
+}
 
-const config = {
-  s3Client: awsS3Client
-};
-const client = s3.createClient(config);
+function invalidateCache() {
+  return new Promise((resolve, reject) => {
+    const cloudfront = new AWS.CloudFront();
+    const reference = Date.now();
 
-const uploader = client.uploadDir({
-  localDir: "public",
-  deleteRemoved: true,
-  s3Params: {
-    Bucket: process.env.BUCKET
-  }
-});
-uploader.on('error', function(err) {
-  console.error(err);
-  process.exit(1)
-});
-uploader.on('progress', function() {
-  if (process.env.CIRCLECI !== 'true') {
-    const percent = `${((uploader.progressAmount / uploader.progressTotal) * 100).toFixed(2) || 0}%`;
-    console.log(percent);
-  }
-});
-uploader.on('end', function() {
-  console.log("done!");
-});
+    const invalidation = {
+      DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
+      InvalidationBatch: {
+        CallerReference: reference.toString(),
+        Paths: {
+          Quantity: 1,
+          Items: [
+            '/*'
+          ]
+        }
+      }
+    };
+    cloudfront.createInvalidation(invalidation, (err, data) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(data);
+        resolve();
+      }
+    });
+  })
+}
 
-/* invalidate old common files */
-
-const reference = Date.now();
-
-const cloudfront = new AWS.CloudFront();
-const invalidation = {
-  DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
-  InvalidationBatch: {
-    CallerReference: reference.toString(),
-    Paths: {
-      Quantity: 1,
-      Items: [
-        '/*'
-      ]
-    }
-  }
-};
-cloudfront.createInvalidation(invalidation, (err, data) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  } else {
-    console.log(data);
-    process.exit(0);
-  }
-});
+uploadFiles()
+.then(invalidateCache)
+.catch(e => console.error)
+.then(() => process.exit(0));
