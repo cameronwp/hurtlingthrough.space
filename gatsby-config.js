91007d9c706d2e5f07d11d594196f3cfab28ad6c
cameronwp <cameron.w.pittman@gmail.com>
2017-12-07 22:17:50 -0600

    add sass support

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
     `gatsby-plugin-feed`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: 'gatsby-plugin-typography',
       options: {
