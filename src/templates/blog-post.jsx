90e359117fb9538be9d513fcf7f49c40301457dc
cameronwp <cameron.w.pittman@gmail.com>
2017-12-09 13:09:48 -0600

    improve tag display

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -10,7 +10,7 @@ import './blog-post.scss'
 
 function renderTag(tag, index) {
   return (
-    <a key={index} href='' className='tag'>{tag}</a>
+    <div className='tag'><a key={index} href='' className='tag-content'>{tag}</a></div>
   )
 }
 
@@ -20,26 +20,38 @@ class BlogPostTemplate extends React.Component {
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
     const {date, title, tags} = post.frontmatter
 
+    const sectionStyle = {
+      ...scale(-0.2),
+      display: 'block',
+      marginBottom: rhythm(0.8), // subtract a little to account for .tag margin-bottom
+      marginTop: rhythm(-0.5)
+    }
+
     return (
       <div>
         <Helmet title={`${title} | ${siteTitle}`} />
         <h1>{title}</h1>
-        <section style={{
-          ...scale(-0.2),
-          display: 'flex',
-          marginBottom: rhythm(1),
-          marginTop: rhythm(-0.5),
-        }}>
-          <div>{date}</div>
-          <div className='tags'>
-            {tags.map(renderTag)}
+        <section style={sectionStyle}>
+          <div className='meta-info'>
+            <div className='date'>{date}</div>
+            <div className='tags'>
+              <div className='flex-row'>
+                {tags.map(renderTag)}
+              </div>
+            </div>
           </div>
         </section>
+
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+
         <section className='share-section'>
           <div className='social'>
             <Share url={window.location.href}
-              options={{ size: 'small', text: `Check out '${title}'`, via: 'cwpittman' }}
+              options={{
+                size: 'small',
+                text: `Check out '${title}'`,
+                via: 'cwpittman'
+              }}
             />
           </div>
           <p className='prompt'>Let's keep the conversation going!</p>
