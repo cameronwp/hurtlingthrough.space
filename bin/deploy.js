8e0a12f12c5a70f88174f331e84c7cc12aa57c76
cameronwp <cameron.w.pittman@gmail.com>
2017-12-10 12:18:07 -0600

    improve caching

--- bin/deploy.js
+++ bin/deploy.js
@@ -15,27 +15,6 @@ AWS.config.apiVersions = {
 };
 AWS.config.update({region: process.env.AWS_REGION});
 
-let filesChanged = [];
-
-function getFileDiffList() {
-  return new Promise((resolve, reject) => {
-    let data = '';
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
-
-    process.stdin.on('data', chunk => {
-      data += chunk;
-    });
-
-    process.stdin.on('end', () => {
-      filesChanged = data.split('\n');
-      resolve();
-    });
-
-    process.stdin.on('error', reject);
-  })
-}
-
 function uploadFiles() {
   return new Promise((resolve, reject) => {
     const awsS3Client = new AWS.S3();
@@ -51,24 +30,17 @@ function uploadFiles() {
         Bucket: process.env.BUCKET
       },
       getS3Params: (localFile, stat, callback) => {
-        callback(null, {
-          CacheControl: 'max-age=691200'  // 8 days
-        });
+        // cache HTML files (and rss.xml) for an hour, everything else 14 days
+        const isHTML = _.endsWith(localFile, '.html')
+        const isXML = _.endsWith(localFile, '.xml')
+        const params = {
+          CacheControl: `max-age=${(isHTML || isXML ? '3600' : '1209600')}`
+        }
+        callback(null, params);
       }
     });
-    uploader.on('error', function(err) {
-      console.error(err);
-      reject(err);
-    });
-    uploader.on('progress', function() {
-      if (process.env.CIRCLECI !== 'true') {
-        const percent = `${((uploader.progressAmount / uploader.progressTotal) * 100).toFixed(2) || 0}%`;
-        console.log(percent);
-      }
-    });
-    uploader.on('end', function() {
-      resolve();
-    });
+    uploader.on('error', reject);
+    uploader.on('end', () => resolve());
   })
 }
 
@@ -77,29 +49,13 @@ function invalidateCache() {
     const cloudfront = new AWS.CloudFront();
     const reference = Date.now();
 
-    // get posts, tags that changed
-    const devPath = 'src/pages'
-    const postsChanged = filesChanged.filter(f => {
-      return _.startsWith(f, devPath)
-    }).map(f => {
-      return f.replace(devPath, '').replace('index.md', 'index.html');
-    });
-
-    const objectsToInvalidate = _.union(postsChanged, [
-      '/index.html',
-      '/offline-plugin-app-shell-fallback/index.html',
-      '/rss.xml'
-    ], (postsChanged.length > 0 ? ['/tags/*'] : []))
-
-    console.log('invalidating: ', objectsToInvalidate);
-
     const invalidation = {
       DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
       InvalidationBatch: {
         CallerReference: reference.toString(),
         Paths: {
-          Quantity: objectsToInvalidate.length,
-          Items: objectsToInvalidate
+          Quantity: 1,
+          Items: ['/*'] // just assume everything has been invalidated
         }
       }
     };
@@ -114,8 +70,7 @@ function invalidateCache() {
   })
 }
 
-getFileDiffList()
-  .then(uploadFiles)
+uploadFiles()
   .then(invalidateCache)
   .catch(e => console.error)
   .then(() => process.exit(0));
--- src/pages/posts/20171210-controlling-caches/index.md
+++ src/pages/posts/20171210-controlling-caches/index.md
@@ -1,7 +1,7 @@
 ---
 title: Controlling Caches
 date: 2017-12-10T12:01:29-06:00
-tags: 
+tags:
   - caching
   - browsers
   - aws
@@ -10,4 +10,7 @@ tags:
 draft: true
 ---
 
-type something smart here
+* deleteRemoved: false
+* cache-control: max-age
+* invalidating
+* browsers kept old versions, which is totally fine. but the cloudfront caches had been killed and the files were deleted from s3. so requests from old versions died
