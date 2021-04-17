74fa5c967fe8cd697001801810f8e8d4342e1951
Cameron Pittman <cameron.w.pittman@gmail.com>
2021-04-17 15:54:28 -0400

    init bibtex component

---
+++ src/components/bibtex.jsx
@@ -0,0 +1,16 @@
+export default function BibTeX() {
+  return <>
+    How to cite this blog post:
+
+    <pre>
+      @misc{Pittman}{Year}
+        title={ },
+        author={Cameron Pittman},
+        url={ },
+        journal={Hurtling through Space},
+        year={ },
+        month={ }
+      }
+    </pre>
+  </>
+}
